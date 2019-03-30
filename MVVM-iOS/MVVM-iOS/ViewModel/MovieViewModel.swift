780bb309af1cf20b315d5ca1daa59668eb0ed9d6
Sagar More <sagarmore62@gmail.com>
2019-03-30 13:01:53 +0530

    view model updated.

--- MVVM-iOS/MVVM-iOS/ViewModel/MovieViewModel.swift
+++ MVVM-iOS/MVVM-iOS/ViewModel/MovieViewModel.swift
@@ -43,10 +43,11 @@ class MovieViewModel {
 struct MovieObjectViewModel {
     let imagePath : String
     let description : NSMutableAttributedString
+    private let model : MovieObject
     
     ///Created dependency injection for view model
     init(_ model : MovieObject) {
-        
+        self.model = model
         //format full image path
         imagePath = Constants.domainImage + "w300" + model.poster_path
         //format description of movie as : movie title (vote average)
@@ -55,5 +56,10 @@ struct MovieObjectViewModel {
         let attributedVote = NSAttributedString(string: " (" + model.vote_average.description  + ")", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 12.0)])
         attributedString.append(attributedVote)
         description = attributedString
+    }
+    
+    /// Get model data for analytics event, if needed.
+    func getModel() -> MovieObject {
+        return model
     }
 }
